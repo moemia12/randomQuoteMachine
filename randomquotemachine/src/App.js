0c10c6bf5cca737503f2fa0cff09b575cb00ea13
Mohsin Miah <moemia12@gmail.com>
2021-01-30 03:59:20 +0000

    Built component with random quote function

--- randomquotemachine/src/App.js
+++ randomquotemachine/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import Button from './components/Button';
+import { random } from 'lodash';
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      quotes: [],
+      selectedQuoteIndex: null,
+    }
+    this.selectQuoteIndex = this.selectQuoteIndex.bind(this);
+  }
+  nextQuoteClickHandler(){
+    
+  }
+
+componentDidMount(){
+  fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+  .then(data => data.json())
+  .then(quotes => this.setState({quotes}, () =>{
+    this.setState({selectedQuoteIndex: this.selectQuoteIndex()})
+  }))
+}
+
+get selectedQuote(){
+  if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)){
+    return;
+  }
+
+  return this.state.quotes[this.state.selectedQuoteIndex];
+}
+
+selectQuoteIndex(){
+  if(!this.state.quotes.length){
+    return;
+  }
+  return random(0, this.state.quotes.length -1)
+}
+
+  render(){
+    console.log(this.state.selectedQuoteIndex);
+    return (
+      <div className="App" id="quote-box">
+          {this.selectedQuote ? `"${this.selectedQuote.quote}" - ${this.selectedQuote.author}` : ''}
+          <Button buttonDisplayName="Next Quote" onClick={this.nextQuoteClickHandler()}/>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
+
+ 
