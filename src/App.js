3e2888af7a4b691884d0e0c8b7f9299c26a3defa
Mohsin Miah <moemia12@gmail.com>
2021-02-04 02:05:35 +0000

    Material UI added

--- src/App.js
+++ src/App.js
@@ -1,61 +1,75 @@
 import { random } from 'lodash';
-import './App.css';
+import 'typeface-roboto';
 import React, { Component } from 'react'
 import QuoteMachine from './components/QuoteMachine'
+import { Grid, withStyles } from '@material-ui/core'
 
 
-// Global App class for the Random Quote Machine
+const styles = {
+  container: {
+    display: 'flex',
+    height: '100vh',
+    alignItems: 'center'
+  }
+}
+
+// Global App class for the Random Quote Machine - Setting current states for "quotes" & "selectedQuoteIndex"
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       quotes: [],
       selectedQuoteIndex: null,
     }
+
+    //Binding methods
     this.assignNewQuoteIndex = this.assignNewQuoteIndex.bind(this);
     this.generateNewQuoteIndex = this.generateNewQuoteIndex.bind(this);
-    
+
   }
 
-// Extracting Random quotes Data from gist via GitHub
-componentDidMount(){
-  fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
-  .then(data => data.json())
-  .then(quotes => this.setState({quotes}, this.assignNewQuoteIndex));
-}
-
-//Function that gets the random selected quote
-get selectedQuote(){
-  if(!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)){
-    return undefined;                                                                                 {/*Checking to see if the quotes has a length OR whether the index exists */}                                                                                {/*Return undefined if not*/}
-  } 
-  return this.state.quotes[this.state.selectedQuoteIndex];                                            {/*Else return the selected quote */}
-}
-
-//Method to generate the quote index by returning an interger from state.quotes (Using Lodash for the Random func)
-generateNewQuoteIndex(){
-  if(!this.state.quotes.length){
-    return undefined;
+  // Extracting Random quotes Data from gist via GitHub
+  componentDidMount() {
+    fetch('https://gist.githubusercontent.com/natebass/b0a548425a73bdf8ea5c618149fe1fce/raw/f4231cd5961f026264bb6bb3a6c41671b044f1f4/quotes.json')
+      .then(data => data.json())
+      .then(quotes => this.setState({ quotes }, this.assignNewQuoteIndex));
   }
-  return random(0, this.state.quotes.length -1)
-}
 
-// Method to assign the index quote to the Button component below
-assignNewQuoteIndex(){
-  this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex()});
+  //Function that gets the random selected quote
+  get selectedQuote() {
+    if (!this.state.quotes.length || !Number.isInteger(this.state.selectedQuoteIndex)) {
+      {/*Checking to see if the quotes has a length OR whether the index exists */ }
+      return undefined; {/*Return undefined if not*/ }
+    }
+    return this.state.quotes[this.state.selectedQuoteIndex]; {/*Else return the selected quote */ }
+  }
 
-}
+  //Method to generate the quote index by returning an interger from state.quotes (Using Lodash for the Random func)
+  generateNewQuoteIndex() {
+    if (!this.state.quotes.length) {
+      return undefined;
+    }
+    return random(0, this.state.quotes.length)
+  }
 
-  render(){
+  // Method to assign the index quote to the Button component below
+  assignNewQuoteIndex() {
+    this.setState({ selectedQuoteIndex: this.generateNewQuoteIndex() });
+
+  }
+
+  render() {
     return (
-      <div className="App" id="quote-box">
-          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex}/>
-      </div>
+      <Grid className={this.props.classes.container} id="quote-box" justify='center' container>
+        <Grid xs={11} lg={8} item>
+          <QuoteMachine selectedQuote={this.selectedQuote} assignNewQuoteIndex={this.assignNewQuoteIndex} />
+        </Grid>
+
+      </Grid>
     );
   }
-  
+
 }
 
-export default App;
+export default withStyles(styles)(App);
 
- 
--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -1,11 +1,27 @@
+import { Typography } from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
 import React from 'react';
-import Button from './Button';
 
-const QuoteMachine = (props) =>(
- <React.Fragment>
-    {props.selectedQuote ? `"${props.selectedQuote.quote}" - ${props.selectedQuote.author}` : ''} 
-    <Button buttonDisplayName="Next Quote" clickHandler={props.assignNewQuoteIndex}/>
- </React.Fragment>
+
+const QuoteMachine = (props) => (
+   <Card>
+      <CardContent>
+         {
+            props.selectedQuote ? (
+               <Typography>
+                  {props.selectedQuote.quote} - {props.selectedQuote.author}
+               </Typography>
+            ) : null
+         }
+      </CardContent>
+      <CardActions>
+      <Button size={'small'} onClick={props.assignNewQuoteIndex}>Next Quote</Button>
+      </CardActions>
+      
+   </Card>
 );
 
 
