184f22affd056d4a276407eb8d400ec02d3365cc
Mohsin Miah <moemia12@gmail.com>
2021-02-04 23:21:20 +0000

    UI Change

--- src/components/QuoteMachine.js
+++ src/components/QuoteMachine.js
@@ -11,18 +11,21 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 
 const QuoteMachine = ({ backgroundQuoteChange, selectedQuote }) => (
-   <Card style={{ padding: '2rem' , height: '30rem', position: 'relative', top: '5rem', background: 'linear-gradient(0deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,1) 100%)' }}>
+   <Card style={{ padding: '2rem' , height: '30rem', position: 'relative', top: '5rem', background: 'linear-gradient(0deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,1) 100%)', boxShadow: 'box-shadow: 0px 5px 9px 1px rgba(0,0,0,0.75);' }}>
       <CardContent>
 
          <Typography style={{ fontSize: 40 }} id="text">
-            {selectedQuote.quote} - <span id='author'>{selectedQuote.author}</span>
+            {selectedQuote.quote} 
+            <br></br>
+            <br></br>
+            - <span id='author'>{selectedQuote.author}</span>
          </Typography>
 
       </CardContent>
       <CardActions>
-         <Button style={{ fontSize: 20, position: 'relative', top: '10rem', left: '5rem', marginTop: '-10rem' }} id="new-quote" size={'small'} onClick={backgroundQuoteChange}>Next Quote</Button>
+         <Button style={{ fontSize: 20, position: 'relative', top: '10rem', left: '40rem', marginTop: '-10rem', background: 'linear-gradient(0deg, rgba(255,255,255,0.5) 0%, rgba(255,255,255,1) 100%)' }} id="new-quote" size={'small'} onClick={backgroundQuoteChange}>Next Quote</Button>
          <IconButton id="tweet-quote" target="_blank" href={encodeURI(`https://twitter.com/intent/tweet?text=${selectedQuote.quote}`)}>
-            <FontAwesomeIcon style={{ position: 'relative', top: '10rem', left: '5rem', marginTop: '-10rem' }} icon={faTwitter} size='lg'></FontAwesomeIcon>
+            <FontAwesomeIcon style={{ position: 'relative', top: '10rem', left: '40rem', marginTop: '-10rem' }} icon={faTwitter} size='lg'></FontAwesomeIcon>
          </IconButton>
       </CardActions>
 
